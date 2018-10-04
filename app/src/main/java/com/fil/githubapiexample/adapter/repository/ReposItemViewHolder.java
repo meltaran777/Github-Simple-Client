bcfb60b68e06ca3382b5893bc1f25e8b05fa0391
meltaran777 <meltaran777@mail.ru>
2018-10-04 12:24:36 +0300

    Fix multiple recycler clicks.Add popup menu for repository recycler item

--- app/src/main/java/com/fil/githubapiexample/adapter/repository/ReposItemViewHolder.java
+++ app/src/main/java/com/fil/githubapiexample/adapter/repository/ReposItemViewHolder.java
@@ -1,8 +1,8 @@
 package com.fil.githubapiexample.adapter.repository;
 
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fil.githubapiexample.R;
@@ -10,19 +10,21 @@ import com.fil.githubapiexample.model.Repository;
 import com.fil.githubapiexample.util.Utilities;
 
 public class ReposItemViewHolder extends RecyclerView.ViewHolder {
+    private static final long CLICK_TIME_INTERVAL = 300;
+    private long mLastClickTime = System.currentTimeMillis();
 
     private View layout;
 
-    private TextView  nameTextView;
-    private TextView  descTextView;
-    private ImageView menuImageView;
+    private TextView nameTextView;
+    private TextView descTextView;
+    private TextView menuTextView;
 
     public ReposItemViewHolder(View v) {
         super(v);
         layout = v;
         nameTextView = v.findViewById(R.id.repo_name_text_view);
         descTextView = v.findViewById(R.id.repo_desc_text_view);
-        menuImageView = v.findViewById(R.id.repo_menu_image_view);
+        menuTextView = v.findViewById(R.id.repo_menu_text_view);
     }
 
     public void bindViews(Repository repository, ReposItemInteractionListener listener) {
@@ -34,7 +36,28 @@ public class ReposItemViewHolder extends RecyclerView.ViewHolder {
             descTextView.setText(description);
         else descTextView.setText(layout.getContext().getString(R.string.no_desc_text));
 
-        layout.setOnClickListener(view ->
-                listener.onReposItemClicked(repository, getAdapterPosition()));
+        layout.setOnClickListener(view -> {
+            long now = System.currentTimeMillis();
+            if (now - mLastClickTime < CLICK_TIME_INTERVAL) {
+                return;
+            }
+            mLastClickTime = now;
+            listener.onReposItemClicked(repository, getAdapterPosition());
+        });
+
+        menuTextView.setOnClickListener(view -> {
+            PopupMenu popup = new PopupMenu(menuTextView.getContext(), menuTextView);
+            popup.inflate(R.menu.repository_item_popup_menu);
+            popup.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.delete_action:
+
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+            popup.show();
+        });
     }
 }
